4a30ea08d9477e7d3a417d1a538ad48abb2d85cd
Moustapha Akanmu <MAkanmu@spartaglobal.com>
2019-10-02 10:51:39 +0100

    running examples queries

--- db_connect.py
+++ db_connect.py
@@ -13,6 +13,48 @@ conn_northwind = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='
 print(conn_northwind)
 
 # create a cursor
+#Allow us to execute readonly queries on the database
+cursor = conn_northwind.cursor()
+
+#using .execute() on cursor
+#cursor.execute("SELECT @@version;")
+
+#using .execute() on cursor
+cursor.execute("SELECT * FROM Customers;")
+
+#Fetch rows from cursor - fetchione
+row = cursor.fetchone()
+print(row)
+
+
+# .fecthall()
+ #this is bad practise
+ # We don't use this
+rows = cursor.execute("SELECT  * FROM Customers").fetchall()
+print(rows)
+#rows = cursor.fetcall()
+print(len(rows))
+print(type(rows)) # if this is a list, we can iterate
+
+# Fetch one data using for loop
+
+rows_4 = cursor.execute("SELECT * FROM Products").fetchall()
+# we can iterate
+for record in rows_4:
+    print(type(record))
+    print (record.UnitPrice) # We can access the column of a epcific record
+    #howerve this is dangerous as we can clog our machine with loads of date
+    #hence we can use while Loop to be more efficient
+    # not a good practise as there is a limited amount of ram 
+
+rows_5 = cursor.exceute("SELECT * FROM Products")
+while True:
+    record = rows.fetchone()
+    if record is None: # once it reaches the end
+        break
+    print(record.UnitPrice)
+
+
 
 #Fetch some data
 
